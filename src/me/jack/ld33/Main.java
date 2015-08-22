5475003ee8acb4b21010d0f44074b9fb32f113e5
Jack Patrick <jackpatrickmail@gmail.com>
2015-08-22 02:56:16 +0100

    Added a Room class and two tiles.

--- src/me/jack/ld33/Main.java
+++ src/me/jack/ld33/Main.java
@@ -1,18 +1,16 @@
 package me.jack.ld33;
 
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
 import uk.co.jdpatrick.JEngine.JEngine;
 
 /**
  * Created by Jack on 05/12/2014.
  */
 public class Main{
-    public static void main(String[] args){
 
+    public static void main(String[] args){
+        JEngine.start(new LD33Game("?????? For LD33 -- Theme: You Are The Monster"));
     }
+
 }
 
