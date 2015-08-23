b691b5c0fcda7a785c59bb8a723e04858f883c44
Jack Patrick <jackpatrickmail@gmail.com>
2015-08-24 00:07:31 +0100

    Added different player textures depending on the direction of travel.

--- src/me/jack/ld33/Entity/MobPlayer.java
+++ src/me/jack/ld33/Entity/MobPlayer.java
@@ -7,6 +7,8 @@ import me.jack.ld33.States.InGameState;
 import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.geom.Circle;
 import uk.co.jdpatrick.JEngine.Image.ImageUtil;
 
@@ -24,15 +26,25 @@ public class MobPlayer extends Mob {
     WeaponInventory weaponInventory = new WeaponInventory();
     public int selectedWeaponSlot = 0;
 
-    public static Image playerSprite;
+    public static SpriteSheet playerSprites;
 
     public HashMap<ProjectileType, Integer> ammo = new HashMap<ProjectileType, Integer>();
 
+    private int facing = 0;//NORTH = 0,EAST = 1,SOUTH = 2,EAST = 3
+    private Image[] facingImages = new Image[4];
     public MobPlayer(int x, int y) {
         super(x, y, 32, 32);
-        if (playerSprite == null) {
-            playerSprite = ImageUtil.loadImage("res/player.png");
-            playerSprite.setCenterOfRotation(16, 16);
+        if (playerSprites == null) {
+            try {
+                playerSprites = new SpriteSheet("res/player.png",32,32);
+                facingImages[0] = playerSprites.getSprite(1,0);
+                facingImages[1] = playerSprites.getSprite(0,1);
+                facingImages[2] = playerSprites.getSprite(0,0);
+                facingImages[3] = playerSprites.getSprite(1,1);
+            } catch (SlickException e) {
+                e.printStackTrace();
+            }
+
         }
         this.health = 50f;
         this.maxHealth = 50f;
@@ -47,21 +59,29 @@ public class MobPlayer extends Mob {
     public void update(Level level, float delta) {
 
         if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            if (level.canMove(getX(), getY() - 8, getWidth(), getHeight(), this))
+            if (level.canMove(getX(), getY() - 8, getWidth(), getHeight(), this)) {
+                facing =0;
                 addY(-8);
+            }
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-            if (level.canMove(getX(), getY() + 8, getWidth(), getHeight(), this))
+            if (level.canMove(getX(), getY() + 8, getWidth(), getHeight(), this)){
+                facing = 2;
                 addY(8);
-
+            }
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            if (level.canMove(getX() - 8, getY(), getWidth(), getHeight(), this))
+            if (level.canMove(getX() - 8, getY(), getWidth(), getHeight(), this)){
+                facing = 3;
                 addX(-8);
+            }
+
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            if (level.canMove(getX() + 8, getY(), getWidth(), getHeight(), this))
+            if (level.canMove(getX() + 8, getY(), getWidth(), getHeight(), this)) {
+                facing = 1;
                 addX(8);
+            }
         }
 
 
@@ -110,7 +130,7 @@ public class MobPlayer extends Mob {
     @Override
     public void render(Graphics graphics) {
         //   graphics.fill(attractRadius);
-        graphics.drawImage(playerSprite, getX(), getY());
+        graphics.drawImage(facingImages[facing], getX(), getY());
 
 
     }
